d306e65f0eb663bc5507cf0139014828e7c03689
arunrajendiran <57187845+arunrajendiran@users.noreply.github.com>
2020-02-05 08:22:29 +0530

    ex3.rb
    ex3

---
+++ ex3.rb
@@ -0,0 +1,6 @@
+puts "now we will count the members in my team"
+puts  "total member #{2+6} "
+puts "total girls #{1+1}"
+puts "total boys #{3+3}"
+puts "Is it true that #{3+3} < #{1+1}?"
+puts 3+3 < 1+1
